0fe6491a8b7aebc086bc49ca89a0276e6d57ae9d
Tatsuya Ueda <ml@tatsuya.info>
2017-03-14 22:52:46 +0900

    ・日本語の言語ファイルを追加

---
+++ resources/lang/ja/pagination.php
@@ -0,0 +1,18 @@
+<?php
+
+// from https://github.com/caouecs/Laravel-lang/blob/master/src/ja/pagination.php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+    'previous' => '&laquo; 前',
+    'next' => '次 &raquo;',
+];
