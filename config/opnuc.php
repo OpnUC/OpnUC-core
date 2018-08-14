a53068e05bc2a6fc23e2960c6560fa3714691aa9
Tatsuya Ueda <ml@tatsuya.info>
2018-08-14 16:50:50 +0900

    - 不要な設定値を削除

--- config/opnuc.php
+++ config/opnuc.php
@@ -13,22 +13,6 @@ return [
         //// PBX種類
         // Asterisk: Asterisk
         'type' => env('PBX_LINKER_TYPE'),
-        // typeがAsteriskの場合の設定
-        'asterisk' => [
-            'host' => env('ASTERISK_HOST', '127.0.0.1'),
-            'port' => env('ASTERISK_PORT', 5038),
-            'username' => env('ASTERISK_USERNAME', 'opnuc'),
-            'password' => env('ASTERISK_PASSWORD', 'opnuc'),
-            // Click to Callで使用するコンテキスト
-            'originate_context' => env('ASTERISK_ORIG_CONTEXT', 'opnuc'),
-            // Click to Callで通知するCallerID
-            'originate_callerid' => env('ASTERISK_ORIG_CALLERID', 'Click 2 Call'),
-            // Click to Callで使用するチャンネルのPrefix
-            'originate_channel_prefix' => env('ASTERISK_ORIG_CHAN_PREFIX', ''),
-            // DeviceNameのPrefix(DeviceStateにて使用)
-            //  [Prefix]内線番号 の形式
-            'device_name_prefix' => env('ASTERISK_DEVICE_PREFIX', ''),
-        ],
     ],
 
     //// Click to Callの利用
