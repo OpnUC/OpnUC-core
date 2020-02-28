0da1544b2d518eeda01d085bd03d1c3e85de5abc
Tatsuya Ueda <ml@tatsuya.info>
2020-02-28 09:33:27 +0900

    - 認証のdefault guardを再度見直し(基本はapiとし、必要な箇所だけwebに変更)

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,15 @@
 |
 */
 
-// ログイン情報を復元
+/**
+ * ログイン情報を復元
+ * - Session情報からJWT Tokenを生成し、ログイン情報を引き継ぐ
+ * - この部分だけ、Sessionを利用するため、Middlewareにauth:webを設定
+ */
 Route::get('/loginRestore', function () {
     // ToDo エラー処理
     return redirect()->away('/login?mode=restore&token='. auth('api')->login(\Auth::user()));
-});
+})->middleware('auth:web');
 
 // Vue向けのルート
 // extensions/ broadcasting/ で始まるリクエストは処理しない
